34e7cf24c4320dff860bb3c6a8da7d1feb43ed2a
Mike Ellery <mellery@gmail.com>
2024-12-30 22:56:15 -0500

    initial commit

---
+++ monster_names.py
@@ -0,0 +1,105 @@
+characters = {
+    b'\x80': "0", # TODO: how to get this programmatically?
+    b'\x81': "1",
+    b'\x82': "2",
+    b'\x83': "3",
+    b'\x84': "4",
+    b'\x85': "5",
+    b'\x86': "6",
+    b'\x87': "7",
+    b'\x88': "8",
+    b'\x89': "9",
+    b'\x8a': "A",
+    b'\x8b': "B",
+    b'\x8c': "C",
+    b'\x8d': "D",
+    b'\x8e': "E",
+    b'\x8f': "F",
+    b'\x90': "G",
+    b'\x91': "H",
+    b'\x92': "I",
+    b'\x93': "J",
+    b'\x94': "K",
+    b'\x95': "L",
+    b'\x96': "M",
+    b'\x97': "N",
+    b'\x98': "O",
+    b'\x99': "P",
+    b'\x9a': "Q",
+    b'\x9b': "R",
+    b'\x9c': "S",
+    b'\x9d': "T",
+    b'\x9e': "U",
+    b'\x9f': "V",
+    b'\xa0': "W",
+    b'\xa1': "X",
+    b'\xa2': "Y",
+    b'\xa3': "Z",
+    b'\xa4': "a",
+    b'\xa5': "b",
+    b'\xa6': "c",
+    b'\xa7': "d",
+    b'\xa8': "e",
+    b'\xa9': "f",
+    b'\xaa': "g",
+    b'\xab': "h",
+    b'\xac': "i",
+    b'\xad': "j",
+    b'\xae': "k",
+    b'\xaf': "l",
+    b'\xb0': "m",
+    b'\xb1': "n",
+    b'\xb2': "o",
+    b'\xb3': "p",
+    b'\xb4': "q",
+    b'\xb5': "r",
+    b'\xb6': "s",
+    b'\xb7': "t",
+    b'\xb8': "u",
+    b'\xb9': "v",
+    b'\xba': "w",
+    b'\xbb': "x",
+    b'\xbc': "y",
+    b'\xbd': "z",
+    b'\xbe': "'",
+    b'\xbf': ",",
+    b'\xc0': ".",
+    b'\xc1': " ",
+    b'\xc2': "-",
+    b'\xc3': "..",
+    b'\xc4': "!",
+    b'\xc5': "?"
+}
+
+
+def get_names(file_bytes):
+    result_string = ""
+    start_index = 0
+    start_offset = 0x2d5f0
+    names = []
+    for index, byte in enumerate(file_bytes[start_offset:], start=start_offset):
+        hex_byte = bytes([byte])
+        if hex_byte in characters:
+            if not result_string:
+                start_index = index
+            result_string += characters[hex_byte]
+        else:
+            if result_string and len(result_string) > 2:
+                names.append(result_string)
+                #print(f"{start_index:#x}: {result_string}")
+                result_string = ""
+                if len(names) == 128:
+                    return names
+            #print(f"{hex_byte.hex()}", end=' ')
+    if result_string and len(result_string) > 2:
+        #print(f"{start_index:#x}: {result_string}")
+        names.append(result_string)
+
+
+# Open the file in binary mode
+with open('Final Fantasy (USA).nes', 'rb') as file:  # TODO: command line argument
+    # Read the entire file as bytes
+    file_bytes = file.read()
+
+# Call the find_strings function
+get_names(file_bytes)
---
+++ print_monsters.py
@@ -0,0 +1,58 @@
+import struct
+from dataclasses import dataclass
+from monster_names import get_names
+
+@dataclass
+class MonsterStats:
+    exp: int
+    gold: int
+    HP: int
+    morale: int
+    unknown1: int  # ai?
+    unknown2: int  # evade?
+    absorb: int
+    hits: int
+    unknown5: int  # hit rate?
+    dmg: int
+    unknown6: int  # crit rate?
+    unknown7: int  # ?
+    unknown8: int  # attack ailment?
+    family_group: int
+    unknown10: int  # magic def?
+    element_weak: int
+    element_resist: int
+
+    def __str__(self):
+        return (
+            f"  ({self.family_group})\n"
+            f"  EXP:{self.exp} \t GLD: {self.gold} \t HP: {self.HP}\n"
+            f"  ABS: {self.absorb} \t HIT: {self.hits} \t DMG: {self.dmg}\n"
+            f"  WEAK:{self.element_weak}\tRESIST:{self.element_resist}\n"
+
+        )
+
+# Define the struct format for MonsterStats
+struct_format = 'HHHBBBBBBBBBBBBBB'
+
+
+# Function to create an instance of the MonsterStats dataclass from file_bytes starting at a given offset
+def create_monster_stats_instance(file_bytes, offset):
+    struct_size = struct.calcsize(struct_format)
+    struct_data = file_bytes[int(offset):int(offset) + struct_size]
+    unpacked_data = struct.unpack(struct_format, struct_data)
+    return MonsterStats(*unpacked_data)
+
+
+# Open the file in binary mode
+with open('Final Fantasy (USA).nes', 'rb') as file:
+    # Read the entire file as bytes
+    file_bytes = file.read()
+
+offset = 0x30530
+struct_size = struct.calcsize(struct_format)
+names = get_names(file_bytes)
+
+# Loop through all 128 monsters
+for x in range(0, 128):
+    monster_stats = create_monster_stats_instance(file_bytes, offset+(x*struct_size))
+    print(names[x], monster_stats)  # Output will be an instance of MonsterStats with the values of the fields
